de2afaef90d2a15d9695ca16f798fe920e97a24b
Sam K <kolhar730@gmail.com>
2019-02-05 19:08:29 +0530

    init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-router-v4-with-drawer
+
+React Boilerplate with React Router v4 and also one component with MDC Top Bar and Drawer with React Router.
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/ReactRouterv4Usage.jsx',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'ReactRouterv4Usage.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
